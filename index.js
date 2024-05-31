834767da7ee8cac75fc0d10689f9051544aaecb9
sabrinabarzoni <sabrinabarzoni@gmail.com>
2024-05-31 14:54:37 +0200

    change let

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-/* esercizio 1 */
+/* esercizio 1 
 
 let variabile1 = 32;
 const variabile2 =50;
 
 console.log(variabile1);
 console.log(variabile2);
+ 
+esercizio 2 */
+
+let n = 5;
+
+console.log(n);
+
+n = 7;
+ 
+console.log(n);
