56ecf47deff9bbf90cb43b90a7042019aa281cac
sabrinabarzoni <sabrinabarzoni@gmail.com>
2024-06-05 11:24:43 +0200

    array + for

--- index.js
+++ index.js
@@ -95,7 +95,16 @@ else(score <= 59){
 
 ?? 
 
-esercizio 12*/
+esercizio 12
 
 let numeri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(numeri)
+
+esercizio 13 */
+
+let numeri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let somma = 0
+ for(let i=0; i < numeri.length; i++){
+    somma += numeri[i];
+    console.log(somma)
+ }
